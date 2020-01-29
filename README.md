6418bd32298321c52e2dd93e9ff5a2d99f770cae
Raquel McLellan <raquelmclellan@gmail.com>
2020-01-28 22:17:40 -0600

    loaded solution to required portion of assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript-challenge
+
+Completed required assignment leveraging javascript to iterate over a set of data and pass thorugh as a table on a webpage.
+
+![alt text](/static/images/capture.png "Screen Capture")
--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+
+console.log(data)
+
+data.forEach(ufo => {
+    console.log(ufo);
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
